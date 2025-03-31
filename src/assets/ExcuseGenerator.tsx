aadfdcdbd96695734883bbbc24a1a2500e32f881
Daro <twojemail@example.com>
2025-03-31 19:18:24 +0200

    Creating variable for future form

--- src/assets/ExcuseGenerator.tsx
+++ src/assets/ExcuseGenerator.tsx
@@ -1,6 +1,14 @@
+import {useState} from "react";
+
 const ExcuseGenerator: React.FC = () => {
 
-    return("hello")
+    const [name, setName] = useState<string>("");
+    const [reason, setReason] = useState<string>("spóźnienie");
+    const [credibility, setCredibility] = useState<number>(5);
+    const [date, setDate] = useState<string>("");
+    const [creativity, setCreativity] = useState<string>("Średnia");
+    const [details, setDetails] = useState<string>("");
+    const [urgent, setUrgent] = useState<boolean>(false);
 }
 
 export default ExcuseGenerator
