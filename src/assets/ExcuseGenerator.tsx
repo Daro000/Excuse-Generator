accfbdec67f9dfe9da49c0825e4746f418327564
Daro <twojemail@example.com>
2025-03-31 19:41:28 +0200

    Creating function to generate excuse

--- src/assets/ExcuseGenerator.tsx
+++ src/assets/ExcuseGenerator.tsx
@@ -9,6 +9,20 @@ const ExcuseGenerator: React.FC = () => {
     const [creativity, setCreativity] = useState<string>("Średnia");
     const [details, setDetails] = useState<string>("");
     const [urgent, setUrgent] = useState<boolean>(false);
+    const [excuseList, setExcuseList] = useState<Array<string>>(() => {
+        const savedExcuses = localStorage.getItem("wymowki");
+        return savedExcuses ? JSON.parse(savedExcuses) : [];
+    });
+
+    function generateExuse(){
+        const newExcuse = `${name}, niestety ${reason}, ponieważ ${details}. Poziom wiarygodności: ${credibility}, data: ${date}, kreatywność: ${creativity}, ${urgent ? "pilne" : "niepilne"}.`;
+        const updatedList = [...excuseList,newExcuse];
+        setExcuseList(updatedList)
+        localStorage.setItem("wymowki", JSON.stringify(updatedList))
+    }
+
+
+
 }
 
 export default ExcuseGenerator
